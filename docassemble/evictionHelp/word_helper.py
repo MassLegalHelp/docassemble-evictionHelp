3b2aa8a6c44b2e09e90215bc871632e376f28aa5
Kendall Garner <garner.k@outlook.com>
2019-01-18 01:21:14 +0000

    More changes

--- docassemble/evictionHelp/word_helper.py
+++ docassemble/evictionHelp/word_helper.py
@@ -1,9 +1,18 @@
-import docassemble.base.util
+from docassemble.base.functions import value
 
 def adjust(item, length=None):
-  if item != None:
-    return str(item or "").ljust(length or 20)
-  return " " * length 
+  try:
+    val = value(item)
+    return str(val).ljust(length or 20)
+  except:
+    return " " * (length or 20)
 
 def adjustMoney(item, length=None):
   return adjust(item, length or 10)
+
+def exists(item):
+  try:
+    val = value(item)
+    return len(str(val).strip()) > 0
+  except:
+    return False
